53787607efccdfaa602a152d0b10346e655ca19d
chandrakumarreddy <chandrapenugonda655@gmail.com>
2022-08-29 17:48:54 +0530

    ignore remix config server

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: 'vercel',
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   ignoredRouteFiles: ['**/.*'],
   // appDirectory: 'app',
   // assetsBuildDirectory: 'public/build',
